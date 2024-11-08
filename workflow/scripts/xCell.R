2f56e963548b9718f0c1db7b24f1a7ca5cd1a179
joyling93 <963034741@qq.com>
2024-11-08 16:15:38 +0800

    bugfix

--- workflow/scripts/xCell.R
+++ workflow/scripts/xCell.R
@@ -5,7 +5,7 @@ library(xCell)
 exprMatrix = read.table(snakemake@input[[1]],header=TRUE,row.names=1, as.is=TRUE)
 
 data=xCellAnalysis(exprMatrix)
-out_dir=snakemake@output[[1]]
+out_dir=dirname(snakemake@output[[1]])
 write.table(data,file=file.path(out_dir,"xCell.csv"))
 library(pheatmap)
 png(file.path(out_dir,"xCell.png"),height=8,width=4)
